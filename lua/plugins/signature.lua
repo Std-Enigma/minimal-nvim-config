536b5bb77b7ae6adc202d1c3e4d7185bf2ec03c9
Std-Enigma <std.ilia.bahrebar@gmail.com>
2025-03-03 15:40:23 +0330

    Add lsp_signature.nvim configuration

---
+++ lua/plugins/signature.lua
@@ -0,0 +1,12 @@
+return {
+	"ray-x/lsp_signature.nvim",
+	opts = {
+		transparency = 40,
+		hint_enable = true,
+		floating_window = true,
+		hint_prefix = { above = "↙ ", current = "← ", below = "↖ " },
+		toggle_key = "<M-x>",
+		select_signature_key = "<M-n>",
+		move_cursor_key = "<M-p>",
+	},
+}
